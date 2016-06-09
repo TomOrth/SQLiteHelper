9a11443d4d2f6173e194535cb930a28eac0ed87c
atf1999 <torth212@gmail.com>
2016-06-08 21:01:01 -0400

    Create Table method made

--- Table.py
+++ Table.py
@@ -1,8 +1,12 @@
 class Table(object):
     def __init__(self, tableName="Simple"):
         self.tableName = tableName
+    def getTableName(self):
+        return self.tableName
     def setConnection(self, conn):
         self.conn = conn
+    def getConnection(self):
+        return self.conn
     def setTable(self, tab):
         self.tab = tab
     def withColumns(self, *columns):
@@ -11,4 +15,12 @@ class Table(object):
     def withDataTypes(self, *dataTypes):
         self.dataTypes = dataTypes
         return self
-
+    def createTable(self):
+        CREATE_TABLE = "CREATE TABLE " + self.getTableName() + "("
+        for x in range(len(self.columns)):
+            CREATE_TABLE += self.columns[x] + " " + self.dataTypes[x]
+            if(x != len(self.columns)-1):
+                CREATE_TABLE += ", "
+        CREATE_TABLE += ");"   
+        self.getConnection().execute(CREATE_TABLE)
+        self.getConnection().close()
