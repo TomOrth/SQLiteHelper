f69358ca9e845fbe89464f75e936c9d490a3e89e
atf1999 <torth212@gmail.com>
2016-06-13 20:51:31 -0400

    Update and delete statements added

--- Table.py
+++ Table.py
@@ -1,18 +1,25 @@
 class Table(object):
+    #tableName
     def getTableName(self):
         return self.tab
+    #sets reference to sqlite3 connection
     def setConnection(self, conn):
         self.conn = conn
+    #returns sqlite3 connection
     def getConnection(self):
         return self.conn
+    #sets tableName
     def setTable(self, tab):
         self.tab = tab
+    #creates columns for table
     def withColumns(self, *columns):
         self.columns = columns
         return self
+    #create data types for columns
     def withDataTypes(self, *dataTypes):
         self.dataTypes = dataTypes
         return self
+    #execute creating table
     def createTable(self):
         CREATE_TABLE = "CREATE TABLE " + self.getTableName() + "("
         for x in range(len(self.columns)):
@@ -21,9 +28,11 @@ class Table(object):
                 CREATE_TABLE += ", "
         CREATE_TABLE += ");"   
         self.getConnection().execute(CREATE_TABLE)
+    #data to insert
     def insert(self, *insertData):
         self.insertData = insertData
         return self
+    #performs the insertion
     def into(self, *columnInserts):
         INSERT = "INSERT INTO " + self.getTableName() + " ("
         for col in range(len(columnInserts)):
@@ -38,38 +47,77 @@ class Table(object):
         INSERT += ")"
         self.getConnection().execute(INSERT, self.insertData)  
         self.getConnection().commit()  
+    #columns to select from
     def select(self, *selection):
         self.selection = selection
         self.status = "norm"
         return self
+    #selection parameter columns
     def where(self, *params):
         self.params = params
         return self
+    #data for condition
     def equals(self, *vals):
         self.vals = vals
-        self.status = "where"
+        if(self.status != "delete" and self.status != "update"):
+            self.status = "where"
         return self
-    def query(self):
+    #data fields to update
+    def update(self, *updateColumns):
+        self.updateColumns = updateColumns
+        return self
+    #data items to use in update
+    def setTo(self, *updateVals):
+        self.updateVals = updateVals
+        self.status = "update"
+        return self
+    def delete(self):
+        self.status = "delete"
+        return self
+    #main execute statement
+    def execute(self):
         if(self.status == "norm"):
-           QUERY = "SELECT "
-           for cols in range(len(self.selection)):
-               QUERY += self.selection[cols]
-               if(cols != len(self.selection) - 1):
-                   QUERY += ", "
-           QUERY += "  from " + self.getTableName() 
-           cursor = self.getConnection().execute(QUERY)
-           return cursor
+            QUERY = "SELECT "
+            for cols in range(len(self.selection)):
+                QUERY += self.selection[cols]
+                if(cols != len(self.selection) - 1):
+                    QUERY += ", "
+            QUERY += " FROM " + self.getTableName() 
+            cursor = self.getConnection().execute(QUERY)
+            return cursor
         elif(self.status == "where"):
-           QUERY = "SELECT "
-           for cols in range(len(self.selection)):
-               QUERY += self.selection[cols]
-               if(cols != len(self.selection) - 1):
+            QUERY = "SELECT "
+            for cols in range(len(self.selection)):
+                QUERY += self.selection[cols]
+                if(cols != len(self.selection) - 1):
+                    QUERY += ", "
+            QUERY += " FROM " + self.getTableName() + " WHERE "
+            for p in range(len(self.params)):
+               QUERY += self.params[p] + "=?"
+               if(p != len(self.params) - 1):
                    QUERY += ", "
-           QUERY += " FROM " + self.getTableName() + " where "
-           for p in range(len(self.params)):
-              QUERY += self.params[p] + "=?"
-              if(p != len(self.params) - 1):
-                  QUERY += ", "
-           print(QUERY)
-           cursor = self.getConnection().execute(QUERY, self.vals)
-           return cursor
+            print(QUERY)
+            cursor = self.getConnection().execute(QUERY, self.vals)
+            return cursor
+        elif(self.status == "update"):
+            QUERY = "UPDATE " + self.getTableName() + " SET "
+            for upCols in range(len(self.updateColumns)):
+                QUERY += self.updateColumns[upCols] + "=?"
+                if(upCols != len(self.updateColumns) - 1):
+                    QUERY += ", "
+            QUERY += " WHERE " 
+            for whereCol in range(len(self.params)):
+                QUERY += self.params[whereCol] + "=?"
+                if(whereCol != len(self.params) - 1):
+                    QUERY += ", "
+            finalParams = self.updateVals + self.vals
+            self.getConnection().execute(QUERY, finalParams)
+            self.getConnection().commit()
+        elif(self.status == "delete"):
+            QUERY = "DELETE FROM " + self.getTableName() + " WHERE "
+            for delClause in range(len(self.params)):
+                QUERY += self.params[delClause] + "=?"
+                if(delClause != len(self.params) - 1):
+                    QUERY += ", "
+            self.getConnection().execute(QUERY, self.vals)
+            self.getConnection().commit()
